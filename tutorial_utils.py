a5525cd1c92b23347e30c7896d58e811ca8e07e1
Samuel Farrens <samuel.farrens@gmail.com>
2019-08-23 11:59:31 +0200

    test

--- tutorial_utils.py
+++ tutorial_utils.py
@@ -1,6 +1,6 @@
 import numpy as np
-import matplotlib
-matplotlib.use("agg")
+# import matplotlib
+# matplotlib.use("agg")
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from pysap import load_transform
